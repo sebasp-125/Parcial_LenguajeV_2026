5dd700e7ca0992e53cf875f8ff0b3b43849a72a1
Juan Sebastián Montaño Pérez <sp6027193@gmail.com>
2026-03-03 07:34:18 -0500

    Add ShoesDbContext and worker model for database integration
    - Created ShoesDbContext class to manage database interactions.
    - Added worker model with properties for id, complete_name, and email.
    - Updated project.nuget.cache to reflect new user paths and package dependencies.

---
+++ Api_Shoes_v1/RealModels/worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api_Shoes_v1.RealModels;
+
+public partial class worker
+{
+    public int id { get; set; }
+
+    public string? complete_name { get; set; }
+
+    public string? email { get; set; }
+}
